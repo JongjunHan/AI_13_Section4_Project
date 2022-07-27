66441dc6cec8771c8c6ab5cb16d3140fa9890046
Your Name <you@example.com>
2022-07-27 13:29:48 +0900

    Project4

---
+++ test.py
@@ -0,0 +1,100 @@
+import pickle
+import joblib
+import numpy as np
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
+import os
+import random 
+import cv2
+import imutils
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelBinarizer
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras import optimizers
+from sklearn.preprocessing import LabelBinarizer
+from keras import backend as K
+from keras.layers import Dense, Activation, Flatten, Dense,MaxPooling2D, Dropout
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.layers import TextVectorization
+import pathlib
+import random
+import string
+import re
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+import sklearn
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+
+ocr = keras.models.load_model('ocr_model.h5')
+
+lbb = LabelBinarizer()
+
+def sort_contours(cnts, method = 'left-to-right'):
+  
+  reverse = False
+  
+  i = 0
+    
+  if method == 'right-to-left' or method == 'bottom-to-top':
+      reverse = True
+    
+  if method == 'top-to-bottom' or method == 'bottom-to-top':
+      i = 1
+    
+  boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+  (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+  key=lambda b:b[1][i], reverse=reverse))
+
+  return (cnts, boundingBoxes)
+
+def get_letters(img):
+    
+  letters = []
+  image = cv2.imread(img)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+  dilated = cv2.dilate(thresh1, None, iterations = 2)
+
+  cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cnts = imutils.grab_contours(cnts)
+  cnts = sort_contours(cnts, method = 'left-to-right')[0]
+
+  for c in cnts:
+    
+    if cv2.contourArea(c) > 10:
+      (x, y, w, h) = cv2.boundingRect(c)
+      cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+      
+    roi = gray[y : y + h, x : x + w]
+    thresh = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh = cv2.resize(thresh, (32, 32), interpolation = cv2.INTER_CUBIC)
+    thresh = thresh.astype('float32') / 255.0
+    thresh = np.expand_dims(thresh, axis = -1)
+    thresh = thresh.reshape(1, 32, 32, 1)
+    ypred = ocr.predict(thresh)
+    ypred = lbb.inverse_transform(ypred)
+    [x] = ypred
+    letters.append(x)
+
+  return letters, image
+
+def get_word(letter):
+  
+  word = ''.join(letter)
+  
+  return word
+
+letter,image = get_letters('test/TEST_010')
+word = get_word(letter)
+
+print(word)
+
+
+
